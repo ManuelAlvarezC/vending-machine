d482d189839ad817840034cb595251dfa808861a
Manuel Alvarez <manuel@abacum.io>
2023-07-25 12:27:42 +0200

    Move types to its own file

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface ProductType {
+	name: string;
+	price: number;
+	quantity: number;
+}
+
+export interface CashType {
+	amount: number;
+}
+
+export interface UserProfileType {
+	id: string;
+	name: string;
+    balance: number
+
+}
