afd158303ace2854ea617a7bd78c6c6c6eb78b50
Manuel Alvarez <manuel@abacum.io>
2023-07-26 10:21:24 +0200

    Add redux scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
 import { Login, NotFound, VendingMachine } from './pages';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { store } from './redux/store';
+import { Provider } from 'react-redux'
 
+ReactDOM.render(
+	<Provider store={store}>
+	  <App />
+	</Provider>,
+	document.getElementById('root')
+  )
+  
 function App() {
   return (
 	<BrowserRouter>
@@ -13,5 +23,7 @@ function App() {
 	</BrowserRouter>
   );
 }
+
+
 
 export default App;
